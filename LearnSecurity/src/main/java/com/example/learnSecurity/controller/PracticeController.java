ea8cdafd7d3616545b6f0fc66fe70c6906dd5d67
zikei <sigetaka1517@gmail.com>
2023-08-30 01:12:17 +0900

    実習アクセス

--- LearnSecurity/src/main/java/com/example/learnSecurity/controller/PracticeController.java
+++ LearnSecurity/src/main/java/com/example/learnSecurity/controller/PracticeController.java
@@ -7,12 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.learnSecurity.data.PracticeInstance;
 import com.example.learnSecurity.data.link.InstanceLinkView;
 import com.example.learnSecurity.entity.Instance;
+import com.example.learnSecurity.exception.ResourceAccessException;
 import com.example.learnSecurity.service.AccountService;
 import com.example.learnSecurity.service.PracticeService;
+
+import jakarta.servlet.http.HttpSession;
 
 /** 実習コントローラ */
 @Controller
@@ -23,6 +28,9 @@ public class PracticeController {
 	
 	@Autowired
 	PracticeService practiceService;
+	
+	@Autowired
+	private HttpSession session;
 	
 /* ====================================================================================================================== */
 	
@@ -37,6 +45,25 @@ public class PracticeController {
 		model.addAttribute("insList", insLinkList);
 		
 		return "InstanceList";
+	}
+	
+	/** 実習アクセス */
+	@GetMapping("/{insId}")
+	public String Practice(@PathVariable Integer insId, Model model ) {
+		PracticeInstance pIns;
+		try {
+			pIns = practiceService.selectPracticeInstance(insId);
+			// アクセスユーザが所有するインスタンス以外へのアクセスの場合エラー
+			if(!accountService.isLoginUser(pIns.getUserId())) new ResourceAccessException("403：Forbidden");
+		} catch (Exception e) {
+			model.addAttribute("errorMsg", "実習情報を取得できませんでした");
+			return "error";
+		}
+		
+		session.removeAttribute("ins");
+		session.setAttribute("ins", pIns);
+		
+		return "redirect:" + pIns.getProjectPath();
 	}
 	
 /* ====================================================================================================================== */
--- LearnSecurity/src/main/java/com/example/learnSecurity/service/PracticeServiceImpl.java
+++ LearnSecurity/src/main/java/com/example/learnSecurity/service/PracticeServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.learnSecurity.data.PracticeInstance;
 import com.example.learnSecurity.entity.Instance;
 import com.example.learnSecurity.entity.Practice;
+import com.example.learnSecurity.exception.NotFoundException;
 import com.example.learnSecurity.repository.InstanceRepository;
 import com.example.learnSecurity.repository.PracticeRepository;
 
@@ -35,5 +37,12 @@ public class PracticeServiceImpl  implements PracticeService{
 		insRepo.findByUserId(userId).forEach(insList::add);
 		
 		return insList;
+	}
+
+	@Override
+	public PracticeInstance selectPracticeInstance(Integer insId) throws NotFoundException {
+		Instance ins      = insRepo.findById(insId).orElseThrow(() -> new NotFoundException("404 InstanceID : " + insId));
+		Practice practice = practiceRepo.findById(ins.getPracticeId()).orElseThrow(() -> new NotFoundException("404 PracticeID : " + insId));
+		return new PracticeInstance(ins, practice);
 	}
 }
