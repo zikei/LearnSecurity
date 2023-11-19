3d505b86d620a452b8ef0582f9022bcc1dee3815
zikei <sigetaka1517@gmail.com>
2023-11-19 22:22:38 +0900

    処理実装

--- LearnSecurity/src/main/java/com/example/learnSecurity/service/AccountServiceImpl.java
+++ LearnSecurity/src/main/java/com/example/learnSecurity/service/AccountServiceImpl.java
@@ -2,20 +2,25 @@ package com.example.learnSecurity.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.learnSecurity.data.LoginInfo;
 import com.example.learnSecurity.repository.AccountRepository;
 
 public class AccountServiceImpl implements AccountService {
+	@Autowired
+	LoginInfo loginInfo;
+	
 	@Autowired
 	AccountRepository accountRepo;
 	
 	@Override
 	public Integer selectLoginUserId() {
-		return 1;
+		return loginInfo.getUserId();
 	}
 
 	@Override
 	public boolean isLoginUser(Integer userId) {
-		return true;
+		if(userId == null) return false;
+		return loginInfo.getUserId() == userId;
 	}
 
 }
