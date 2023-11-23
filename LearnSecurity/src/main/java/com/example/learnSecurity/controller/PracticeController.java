89f312a4303143e60de73a422a7de8f9880b18fb
zikei <sigetaka1517@gmail.com>
2023-11-23 23:25:08 +0900

    修正

--- LearnSecurity/src/main/java/com/example/learnSecurity/controller/PracticeController.java
+++ LearnSecurity/src/main/java/com/example/learnSecurity/controller/PracticeController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,7 +50,7 @@ public class PracticeController {
 	}
 	
 	/** 実習検索　*/
-	@GetMapping
+	@PostMapping
 	public String PracticeSearch(@RequestParam String sword, Model model) {
 		List<Practice>         pracList     = practiceService.searchPractice(sword);
 		List<PracticeLinkView> pracLinkList = makePracticeLinkViewList(pracList);
