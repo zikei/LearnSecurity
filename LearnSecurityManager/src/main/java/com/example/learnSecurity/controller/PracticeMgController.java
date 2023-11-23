e8770656debb7921aa3930f9406d829d0c88244e
zikei <sigetaka1517@gmail.com>
2023-11-23 21:45:17 +0900

    add Controller

---
+++ LearnSecurityManager/src/main/java/com/example/learnSecurity/controller/PracticeMgController.java
@@ -0,0 +1,10 @@
+package com.example.learnSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 実習管理コントローラ */
+@Controller
+@RequestMapping("/LearnSecurityMg/Practice")
+public class PracticeMgController {
+}
---
+++ LearnSecurityManager/src/main/java/com/example/learnSecurity/controller/RelationMgController.java
@@ -0,0 +1,11 @@
+package com.example.learnSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 関係管理コントローラ */
+@Controller
+@RequestMapping("/LearnSecurityMg/Relation")
+public class RelationMgController {
+	
+}
